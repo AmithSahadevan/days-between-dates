8bcd823a112da6e8558622623ae0efd9ff02b62e
Amith S Dev <70939150+AmithSahadevan@users.noreply.github.com>
2021-01-04 22:53:14 +0530

    Update day.py

--- day.py
+++ day.py
@@ -1,7 +1,7 @@
 from datetime import date
 
-first_date = date(2021,1,1)
-last_date = date(2021,1,10)
+first_date = date(2021,1,1) #the first date
+last_date = date(2021,1,10) #the second date
 
 result = last_date-first_date
-print(result.days)
+print(result.days) #printing the number of days.
